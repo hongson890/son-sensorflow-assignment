285ac868769c98935a0dbe5f4d3ef3cbbc28d9a7
son.pham <son.pham@gouranga.net>
2021-02-13 00:56:24 +0700

    -implement sorting for (email, fullName, userName) -adding paging function -update reducer, action

--- src/features/users/users.types.ts
+++ src/features/users/users.types.ts
@@ -1,22 +1,50 @@
-import { SEARCH_USER, SEARCH_USER_SUCCESS } from './users.constants'
+import {
+  SEARCH_USER,
+  SEARCH_USER_CHANGE_ORDER,
+  SEARCH_USER_ERROR,
+  SEARCH_USER_SUCCESS,
+  UPDATE_LOADING,
+} from './users.constants'
 
 export interface SearchUserAction {
-  type: typeof SEARCH_USER,
-  page: number,
-  results: number,
+  type: typeof SEARCH_USER
+  page: number
+  results: number
   seed: string
 }
 export interface SearchUserSuccessAction {
   type: typeof SEARCH_USER_SUCCESS
   payload: any
 }
-export type UsersActionType = SearchUserAction | SearchUserSuccessAction
+export interface SearchUserErrorAction {
+  type: typeof SEARCH_USER_ERROR
+  payload: any
+}
+export interface SearchUserChangeOrderAction {
+  type: typeof SEARCH_USER_CHANGE_ORDER
+  orderBy: string
+  orderDirection: string
+}
+export interface UpdateLoadingAction {
+  type: typeof UPDATE_LOADING
+  isLoading: boolean
+}
+
+export type UsersActionType =
+  | SearchUserAction
+  | SearchUserSuccessAction
+  | SearchUserErrorAction
+  | SearchUserChangeOrderAction
+  | UpdateLoadingAction
 
 export interface UsersState {
   users: {
+    isLoading: boolean
     userList: any[]
     page: number
     results: number
     seed: string
+    orderBy: 'fullName'
+    orderDirection: 'asc'
   }
 }
